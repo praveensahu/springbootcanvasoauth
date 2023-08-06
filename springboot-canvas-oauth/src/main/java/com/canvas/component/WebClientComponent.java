3cd5f01b5f7051dc625ac72360647660c5766919
praveensahu <praveensahuforcareer@gmail.com>
2023-08-06 16:31:30 +0530

    Removing unused logs
    Removing unused logs

--- springboot-canvas-oauth/src/main/java/com/canvas/component/WebClientComponent.java
+++ springboot-canvas-oauth/src/main/java/com/canvas/component/WebClientComponent.java
@@ -1,7 +1,5 @@
 package com.canvas.component;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -10,12 +8,10 @@ import com.canvas.constant.CanvasConstant;
 @Component
 public class WebClientComponent {
 	
-	Logger logger = LoggerFactory.getLogger(WebClientComponent.class);
 	public String getApiResponse(String objectName) throws Exception{
 		String response = "";
 		WebClient webClient = WebClient.builder().build();
 		response = webClient.get().uri(CanvasConstant.url + objectName).header(CanvasConstant.authorization, CanvasConstant.token).retrieve().bodyToMono(String.class).block();
-		System.out.println(response);
 		return response;
 	}
 }
--- springboot-canvas-oauth/src/main/java/com/canvas/service/CourseService.java
+++ springboot-canvas-oauth/src/main/java/com/canvas/service/CourseService.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
